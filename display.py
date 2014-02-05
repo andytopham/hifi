eae0125a2c33f850b88b2d8406172eca9ab72e55
andytopham <andytspare@gmail.com>
2014-02-05 13:21:25 +0000

    Basic display for hifi

--- display.py
+++ display.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 ''' 
   Application to display the song and time on the radio display.
-  
+  Cut out the debug statements by invoking with python -O display.py (for optimisations)
 '''
 import oled
 import time
@@ -17,15 +17,15 @@ myOled.writerow(1,"Radio Display    ")
 
 while True:
 	p = subprocess.check_output(["mpc", "-f", "%artist%", "current"])
-#	print "Artist:",p
 	if p == "":
-	#	p="Stopped"
+		if __debug__: print "Stopped: "+time.strftime("%R")
 		myOled.writerow(2,time.strftime("%R")+"             ")
 		myOled.writerow(1,p+"                ")
 	else:
+		if __debug__: print "Artist:",p.splitlines()[0],
 		myOled.writerow(1,p+"                ")
 		p = subprocess.check_output(["mpc", "-f", "%title%", "current"])
-#		print "Song:",p
+		if __debug__: print "Song:",p.splitlines()[0]
 		myOled.writerow(2,p+"                ")
 	time.sleep(2)
 	
